acb85c62f3d235bdc7ab9a237db7b53db9972898
CharlieMort <61493460+CharlieMort@users.noreply.github.com>
2023-03-16 13:19:49 +0000

    Basic endpoints

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const http = require("http");
 const cors = require("cors");
 const sqlite = require("sqlite3");
 
@@ -8,13 +7,6 @@ const PORT = 5000;
 
 app.use(express.json());
 app.use(cors());
-
-
-
-app.get("/", (req, res) => {
-    console.log("COCK AND BALLS");
-    res.send("MY FUCKING BALLS");
-});
 
 app.get("/players", (req, res) => {
     const db = new sqlite.Database("db.db");
@@ -26,16 +18,47 @@ app.get("/players", (req, res) => {
 
 app.post("/create-player", (req, res) => {
     const db = new sqlite.Database("db.db");
-    db.run(`INSERT INTO Players(name, totalScore) VALUES ("${req.body.name}", "${req.body.score}");`, (err) => {
+    let totalScore = req.body.totalScore?req.body.totalScore:0;
+    db.run(`INSERT INTO Players(name, totalScore) VALUES ("${req.body.name}", ${totalScore});`, (err) => {
         if (err) {
             console.log(err);
-            res.send("no penis");
+            res.send(err);
         }
         else {
-            res.send("big willy moment");
-            console.log("we won son");
+            res.send("Player Added");
         }
     });
+    db.close();
+})
+
+app.post("/add-game", (req, res) => {
+    const db = new sqlite.Database("db.db");
+    let playerID;
+    db.all(`SELECT playerID FROM Players WHERE name="${req.body.name}";`, (err, rows) => {
+        if (!rows[0]) {
+            console.log("player not found " + req.body.name);
+            res.send("Player Not Found");
+            return;
+        } else {
+            playerID = rows[0].playerID;
+            db.run(`INSERT INTO Games(playerID, scoreCard) VALUES(${rows[0].playerID}, "${req.body.scoreCard}")`, (err) => {
+                if (err) {
+                    console.log(err);
+                    res.send("FAILED");
+                    return;
+                }
+            });
+            res.send("Game Added");
+        }
+    });
+    db.close();
+})
+
+app.get("/games", (req, res) => {
+    const db = new sqlite.Database("db.db");
+    db.all(`SELECT * FROM Games;`, (err, rows) => {
+        res.send(rows);
+    })
     db.close();
 })
 
