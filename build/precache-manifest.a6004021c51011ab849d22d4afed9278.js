4da46cec0ebda34197d6595d03ad8ce2ccc8da12
CharlieMort <61493460+CharlieMort@users.noreply.github.com>
2023-03-19 20:49:50 +0000

    actually die

---
+++ build/precache-manifest.a6004021c51011ab849d22d4afed9278.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ffff2e9deb6babb2b75b",
+    "url": "/static/css/main.6273300d.chunk.css"
+  },
+  {
+    "revision": "ffff2e9deb6babb2b75b",
+    "url": "/static/js/main.ffff2e9d.chunk.js"
+  },
+  {
+    "revision": "ff3e70574e23ec862a62",
+    "url": "/static/js/1.ff3e7057.chunk.js"
+  },
+  {
+    "revision": "e89055efdedb5a5a60c5",
+    "url": "/static/js/2.e89055ef.chunk.js"
+  },
+  {
+    "revision": "452ecbd6c2de88b254e0",
+    "url": "/static/js/runtime~main.452ecbd6.js"
+  },
+  {
+    "revision": "ecdc444842e0ddca34be98dfe457d954",
+    "url": "/index.html"
+  }
+];
