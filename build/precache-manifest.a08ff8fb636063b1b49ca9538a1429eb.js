1e285afea1c81f6a00bbffb424304a7f174a6591
CharlieMort <61493460+CharlieMort@users.noreply.github.com>
2023-03-19 15:50:40 +0000

    First Build

---
+++ build/precache-manifest.a08ff8fb636063b1b49ca9538a1429eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7b1c74d65b6df36e7a5",
+    "url": "/static/css/main.0a3a4b82.chunk.css"
+  },
+  {
+    "revision": "d7b1c74d65b6df36e7a5",
+    "url": "/static/js/main.d7b1c74d.chunk.js"
+  },
+  {
+    "revision": "ff3e70574e23ec862a62",
+    "url": "/static/js/1.ff3e7057.chunk.js"
+  },
+  {
+    "revision": "e89055efdedb5a5a60c5",
+    "url": "/static/js/2.e89055ef.chunk.js"
+  },
+  {
+    "revision": "452ecbd6c2de88b254e0",
+    "url": "/static/js/runtime~main.452ecbd6.js"
+  },
+  {
+    "revision": "12021305168e85b0db479e2227ac3a56",
+    "url": "/index.html"
+  }
+];
--- index.js
+++ index.js
@@ -8,9 +8,61 @@ const PORT = 5000;
 app.use(express.json());
 app.use(cors());
 
+function getValueForBowl(bowl) {
+    let bowls = bowl.split("");
+    let score = 0;
+    for (let i = 0; i<bowls.length; i++) {
+        let num = parseInt(bowls[i]);
+        if (num) {
+            score += num;
+        }
+        else {
+            if (bowls[i] == "/") {
+                score = 10;
+            }
+            else if (bowls[i] == "X") {
+                score += 10;
+            }
+        }
+    }
+    return score;
+}
+
+function calculateBowl(scoreCard) {
+    let scores = scoreCard.split(".");
+    let score = 0;
+    console.log(scores);
+    for (let i = 0; i<scores.length; i++) {
+        if (scores[i].length === 3) {
+            score += getValueForBowl(scores[i]);
+        }
+        else if (scores[i][0] == "X") {
+            score += 10;
+            score += getValueForBowl(scores[i+1]);
+            if (scores[i+1] == "X-") {
+                score += getValueForBowl(scores[i+2][0]);
+            }
+            else if (scores[i+1] == "XXX") {
+                score -= 10;
+            }
+        }
+        else if (scores[i][1] === "/") {
+            score += 10;
+            if (i+1 < scores.length) {
+                score += getValueForBowl(scores[i+1][0]);
+            }
+        }
+        else {
+            score += getValueForBowl(scores[i]);
+        }
+        console.log(score);
+    }
+    return score;
+}
+
 app.get("/players", (req, res) => {
     const db = new sqlite.Database("db.db");
-    db.all("SELECT * FROM Players;", (err, rows) => {
+    db.all("SELECT * FROM Players ORDER BY totalScore DESC;", (err, rows) => {
         res.send(rows);
     })
     db.close();
@@ -34,21 +86,31 @@ app.post("/create-player", (req, res) => {
 app.post("/add-game", (req, res) => {
     const db = new sqlite.Database("db.db");
     let playerID;
-    db.all(`SELECT playerID FROM Players WHERE name="${req.body.name}";`, (err, rows) => {
+    db.all(`SELECT * FROM Players WHERE name="${req.body.name}";`, (err, rows) => {
         if (!rows[0]) {
             console.log("player not found " + req.body.name);
             res.send("Player Not Found");
             return;
         } else {
             playerID = rows[0].playerID;
-            db.run(`INSERT INTO Games(playerID, scoreCard) VALUES(${rows[0].playerID}, "${req.body.scoreCard}")`, (err) => {
+            let scoreCard = req.body.scoreCard;
+            let totalScore = calculateBowl(scoreCard);
+            db.run(`INSERT INTO Games(playerID, scoreCard, score, date, gameNum) VALUES(${rows[0].playerID}, "${req.body.scoreCard}", ${totalScore}, "${req.body.date}", ${req.body.gameNum})`, (err) => {
                 if (err) {
                     console.log(err);
                     res.send("FAILED");
                     return;
                 }
+                db.run(`UPDATE Players SET totalScore = ${rows[0].totalScore+totalScore} WHERE playerID = ${playerID};`, (err) => {
+                    if (err) {
+                        console.log(err);
+                        res.send("FAILED but almost got there");
+                        return;
+                    }
+                    res.send("Game Addded");
+                });
             });
-            res.send("Game Added");
+            
         }
     });
     db.close();
@@ -58,7 +120,52 @@ app.get("/games", (req, res) => {
     const db = new sqlite.Database("db.db");
     db.all(`SELECT * FROM Games;`, (err, rows) => {
         res.send(rows);
+        console.log(rows[0]);
     })
+    db.close();
+})
+
+app.get("/games-with-name", (req, res) => {
+    const db = new sqlite.Database("db.db");
+    db.all(`SELECT Games.date, Games.gameID, Games.score, Games.scoreCard, Players.name FROM Games 
+            JOIN Players 
+            ON Games.playerID = Players.playerID 
+            ORDER BY Games.score DESC;`, (err, rows) => {
+                res.send(rows);
+            })
+    db.close();
+})
+
+function updateTotals() {
+    const db = new sqlite.Database("db.db");
+    let scores = {};
+    db.all("SELECT playerID, score FROM Games;", (err, rows) => {
+        rows.map((score) => {
+            if (!scores.hasOwnProperty(score.playerID)) scores[score.playerID] = score.score;
+            else scores[score.playerID] += score.score;
+        })
+        for (const id in scores) {
+            db.run(`UPDATE Players SET totalScore=${scores[id]} WHERE playerID = ${id}`);
+        }
+    })
+    db.close();
+}
+
+app.put("/update-totals", (req, res) => {
+    updateTotals();
+    res.send("Should be donezo");
+});
+
+app.post("/del-game", (req, res) => {
+    console.log(req.body.gameID);
+    const db = new sqlite.Database("db.db");
+    db.run(`DELETE FROM Games WHERE gameID = ${req.body.gameID};`, (err) => {
+        if (err) {
+            console.log(err);
+        }
+    });
+    res.send("Deleted");
+    updateTotals();
     db.close();
 })
 
